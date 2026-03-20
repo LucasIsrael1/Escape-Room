c6da47bd67f5f9b2bbe81a2313d0249a8cb7ab48
Lucas Israel <168013400+LucasIsrael1@users.noreply.github.com>
2026-03-20 17:08:02 +0000

    Better player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
-public class Player : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
-    [SerializeField] private InputActionReference move;
+
+    private InputAction moveAction;
+    
+    [SerializeField] private float speed = 8;
+    [SerializeField] private float acceleration = 8;
     private Vector3 movementInput = new Vector3(0, 0, 0);
-    [SerializeField] private float speed = 2;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    void Update()
-    {
-        Vector2 inputVector = move.action.ReadValue<Vector2>();
-        movementInput.x = inputVector.x * speed;
-        movementInput.y = rb.linearVelocity.y;
-        movementInput.z = inputVector.y * speed;
+    void Update() {
+        movementInput = moveAction.ReadValue<Vector2>();
     }
 
-    void FixedUpdate()
-    {
-        rb.linearVelocity = movementInput;
+    void FixedUpdate() {
+        Vector3 targetVelocity = new Vector3(movementInput.x * speed, 0, movementInput.y * speed);
+        rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * acceleration);
     }
 }
