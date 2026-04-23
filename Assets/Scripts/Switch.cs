477043f39883d6b67a56f5601b50e465b743319a
Lucas Israel <168013400+LucasIsrael1@users.noreply.github.com>
2026-04-23 22:50:14 +0100

    Switch model

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -1,9 +1,13 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
     [SerializeField] private SwitchController controller;
+    
+    [SerializeField] private Material offMaterial;
+    [SerializeField] private Material onMaterial;
+    [SerializeField] private MeshRenderer meshRenderer;
 
     private HashSet<GameObject> overlappingObjects = new HashSet<GameObject>();
 
@@ -12,14 +16,22 @@ public class Switch : MonoBehaviour
         GameObject obj = collider.gameObject;
         if (!obj.CompareTag("Box")) return;
         overlappingObjects.Add(obj);
-        if (overlappingObjects.Count == 1) controller.SwitchPressed();
+        if (overlappingObjects.Count == 1)
+        {
+            controller.SwitchPressed();
+            meshRenderer.material = onMaterial;
+        }
     }
-
-    void OnTriggerExit(Collider collider)
+        
+     void OnTriggerExit(Collider collider)
     {
         GameObject obj = collider.gameObject;
         if (!obj.CompareTag("Box")) return;
         overlappingObjects.Remove(obj);
-        if (overlappingObjects.Count == 0) controller.SwitchUnpressed();
+            if (overlappingObjects.Count == 0)
+            {
+                controller.SwitchUnpressed();
+                meshRenderer.material = offMaterial;
+        }
     }
 }
